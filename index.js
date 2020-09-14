86a600a3b7cd3871a1dc7868eab9e7af7f36be43
chankruze <chankruze@gmail.com>
2020-09-14 19:59:12 +0530

    Adds Store Implementation
    Signed-off-by: chankruze <chankruze@gmail.com>

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ Created: Sun Sep 13 2020 18:17:31 GMT+0530 (India Standard Time)
 
 Copyright (c) Geekofia 2020 and beyond
 */
+const redux  = require('redux')
+// const createStore = redux.createStore
 
 const BUY_CAKE = 'BUY_CAKE'
 
-
-// Action creator
+// action creator
 function buyCake() {
     return {
         type: BUY_CAKE,
@@ -34,3 +35,17 @@ const reducer = (state = initialState, action) => {
                 return state
     }
 }
+
+// 1: holds application state
+// const store = redux.createStore(reducer)
+const store = redux.createStore(reducer)
+// 2: allows access to state via getState()
+console.log('Initial state', store.getState())
+// 4: registers listener via subscriber(listener)
+const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()))
+// 3: allows state to be updated vua dispatch(action)
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+// 5: unsubscribe listener
+unsubscribe()
